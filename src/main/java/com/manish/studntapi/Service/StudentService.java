ad40cc327e58ae31b4445ca3c49507e1fad8b59e
Manish parashar <121606120+ManishParashar0@users.noreply.github.com>
2023-08-20 12:34:01 +0530

    update method build

--- src/main/java/com/manish/studntapi/Service/StudentService.java
+++ src/main/java/com/manish/studntapi/Service/StudentService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.manish.studntapi.entity.Student;
@@ -33,4 +34,22 @@ public class StudentService implements StudentDao {
 		return findById;
 	}
 
+	public ResponseEntity<Object> update(int id, Student updatedStudent) {
+		Optional<Student> findById = repo.findById(id);
+		if (findById.isPresent()) {
+			Student existingStudent = findById.get();
+
+			// Update the fields of the existing student with values from updatedStudent
+			existingStudent.setName(updatedStudent.getName());
+			existingStudent.setDisc(updatedStudent.getDisc());
+			// ... other fields
+
+			repo.save(existingStudent); // Save the updated student
+
+			return ResponseEntity.ok("Update success");
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+
+	}
 }
--- src/main/java/com/manish/studntapi/controller/StudentController.java
+++ src/main/java/com/manish/studntapi/controller/StudentController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,6 +48,12 @@ public class StudentController {
 			return ResponseEntity.notFound().build();
 		}
 
+	}
+
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Object> update(@PathVariable int id, @RequestBody Student updatedStudent) {
+		ResponseEntity<Object> updateResponse = service.update(id, updatedStudent);
+		return updateResponse;
 	}
 
 }
